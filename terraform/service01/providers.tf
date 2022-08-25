0d1dde51ca6dc4d2041ce819cfc9ef545f513021
Alexey_Mayer <alexey_mayer@epam.com>
2022-08-25 17:00:05 +0300

    Add azurerm storage tf backend for state file

--- terraform/service01/providers.tf
+++ terraform/service01/providers.tf
@@ -1,12 +1,19 @@
 terraform {
 
   required_version = ">=1.2.6"
-  
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "=3.19.1"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-gitops-tfstate"
+    storage_account_name = "sagitopstfstate"
+    container_name       = "tfstate"
+    key                  = "service02.tfstate"
   }
 }
 
